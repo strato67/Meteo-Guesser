d8634f73895eaf3f3595dbecbbaef2754905f183
strato67 <saaranshsharma28@gmail.com>
2024-04-22 14:48:45 -0400

    setting up city storage

---
+++ city-storage/src/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({status: 'ok'});
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
