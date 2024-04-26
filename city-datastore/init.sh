9f76868aa3ff49070f8544d1cd5b0d26b94f4739
strato67 <saaranshsharma28@gmail.com>
2024-04-26 13:27:28 -0400

    removing json list

--- city-datastore/init.sh
+++ city-datastore/init.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#wget -O "current.city.list.json.gz" "https://bulk.openweathermap.org/sample/current.city.list.json.gz" 
-
-#gunzip -f "current.city.list.json"
-
-mongoimport --db locations --collection cities --type json --file "./data/current.city.json" --jsonArray
+mongoimport --db locations --collection cities --type json --file "./data/current.city.list.json" --jsonArray
