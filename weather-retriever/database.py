f2d265347000e1dc8852330e67113425b611d463
strato67 <saaranshsharma28@gmail.com>
2024-04-30 14:26:59 -0400

    starting openweather implementation

--- weather-retriever/database.py
+++ weather-retriever/database.py
@@ -1,12 +1,19 @@
 from redis import Redis
 import requests
+import os
+
+api_key = os.environ["OPENWEATHER_API_KEY"] 
 
 def populate_db():
     print("Not implemented yet")
 
-def fetch_weather_data(city):
+def fetch_weather_data(location_list):
+
     print("Not implemented yet")
 
 def get_locations():
-    location_list = requests.get("http://city-gen-service:3000")
-    return location_list.json()
+    response = requests.get("http://city-gen-service:3000").json()
+
+    location_array = response["coordinates"]
+
+    return location_array
