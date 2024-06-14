ffdbd1d49b344b1cbf525d675237795de9b56130
strato67 <saaranshsharma28@gmail.com>
2024-06-14 15:16:06 -0400

    adding join session

--- weather-client/src/components/join-game-card.tsx
+++ weather-client/src/components/join-game-card.tsx
@@ -1,64 +1,108 @@
-import { Label } from "@radix-ui/react-dropdown-menu";
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardContent,
-    CardDescription,
-    CardFooter,
-} from "./ui/card";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "./ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 
+const formSchema = z.object({
+  gamecode: z.string().regex(/^[A-Z][a-z][0-9]{2}[a-z]$/, {
+    message: "Invalid game code.",
+  }),
+});
 
 export default function JoinGameCard() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      gamecode: "",
+    },
+  });
 
-    return (
-        <>
-            <Card className="w-96 bg-secondary ">
-                <CardHeader>
-                    <div className="flex items-baseline gap-2">
-                        <Button variant="link" size="icon" asChild className=" bg-secondary rounded-lg hover:bg-background">
-                            <Link href={"/"}>
-                                <ArrowLeftIcon />
-                            </Link>
-                        </Button>
-                        
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    router.push(`/game/${values.gamecode}`)
+  };
 
-                        <CardTitle className="text-xl">Join a Game</CardTitle>
-                    </div>
+  return (
+    <>
+      <Form {...form}>
+        <Card className="w-96 bg-secondary ">
+          <CardHeader>
+            <div className="flex items-baseline gap-2">
+              <Button
+                variant="link"
+                size="icon"
+                asChild
+                className=" bg-secondary rounded-lg hover:bg-background"
+              >
+                <Link href={"/"}>
+                  <ArrowLeftIcon />
+                </Link>
+              </Button>
 
-                    <CardDescription>
-                        Enter a game code and play with your friends!
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label>Game Code</Label>
-                                <Input
-                                    id="gamecode"
-                                    placeholder="Enter a game code"
-                                    className=" border-primary"
-                                    required
-                                />
-                            </div>
-                        </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="gap-2">
-                    <Button variant={"destructive"} className="w-full py-5 " asChild>
-                        <Link href={"/"}>Cancel</Link>
-                        
-                    </Button>
-                    <Button className="w-full py-5" onClick={() => console.log()}>
-                        Join Game
-                    </Button>
-                </CardFooter>
-            </Card>
-        </>
-    );
+              <CardTitle className="text-xl">Join a Game</CardTitle>
+            </div>
+
+            <CardDescription>
+              Enter a game code and play with your friends!
+            </CardDescription>
+          </CardHeader>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardContent>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <FormField
+                    control={form.control}
+                    name="gamecode"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Game Code</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="enter a game code"
+                            {...field}
+                            className="border-primary"
+                            required
+                            autoComplete="off"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="gap-2">
+              <Button variant={"destructive"} className="w-full py-5 " asChild>
+                <Link href={"/"}>Cancel</Link>
+              </Button>
+              <Button className="w-full py-5" type="submit">
+                Join Game
+              </Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </Form>
+    </>
+  );
 }
--- weather-client/src/components/navbar.tsx
+++ weather-client/src/components/navbar.tsx
@@ -1,37 +1,53 @@
-import Link from "next/link"
-import { ModeToggle } from "./mode-toggle"
+import Link from "next/link";
+import { ModeToggle } from "./mode-toggle";
 
 export default function Navbar() {
-    return (
-        <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 top-0 fixed z-30">
-            
-            <Link className="mr-6 lg:flex" href="/">
-                <MountainIcon className="h-6 w-6" />
-                <span className="sr-only">Acme Inc</span>
-            </Link>
-            <nav className="ml-auto lg:flex gap-6">
-                <ModeToggle />
-
-            </nav>
-        </header>
-    )
+  return (
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 top-0 fixed z-30">
+      <Link className="mr-6 lg:flex" href="/">
+        <LightningIcon className="h-6 w-6" />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <nav className="ml-auto lg:flex gap-6">
+        <ModeToggle />
+      </nav>
+    </header>
+  );
 }
 
-function MountainIcon(props: any) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+function LightningIcon(_props: any) {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <title>lightning [#1262]</title>
+      <desc>Created with Sketch.</desc>
+      <defs></defs>
+      <g
+        id="Page-1"
+        stroke="currentColor"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g
+          id="Dribbble-Light-Preview"
+          transform="translate(-62.000000, -2559.000000)"
+          fill="currentColor"
         >
-            <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-        </svg>
-    )
+          <g id="icons" transform="translate(56.000000, 160.000000)">
+            <polygon
+              id="lightning-[#1262]"
+              points="14 2419 14 2411 6 2411 14 2399 14 2407 22 2407"
+            ></polygon>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
 }
