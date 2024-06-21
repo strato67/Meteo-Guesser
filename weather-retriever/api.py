851874a6170545a8342988c7ec69d3a33bcc50bc
strato67 <saaranshsharma28@gmail.com>
2024-06-20 23:13:47 -0400

    switch to prod

--- weather-retriever/api.py
+++ weather-retriever/api.py
@@ -22,5 +22,5 @@ def get_weather():
 
 if __name__ == '__main__':
     from waitress import serve
-    #serve(app, host="0.0.0.0", port=4200)
-    app.run(host="0.0.0.0", port=4200, debug=True)
+    serve(app, host="0.0.0.0", port=4200)
+    #app.run(host="0.0.0.0", port=4200, debug=True)
